4ba73d7c613f8e9e04a151560c2c898cd2ba5200
Ivaylo763 <iedzhurelov24a@gmail.com>
2026-01-22 09:40:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 name = st.text_input("Въведи име")
-age = st.text_input("Въведи възраст")
-grade = st.text_input("Въведи оценка")
+age = st.number_input("Въведи възраст")
+grade = st.number_input("Въведи оценка")
 
 if age >= 18:
   st.write("Ти си пълнолетен")
