d29eab6d2386ddb92ffcc7973316cb93ac137cb6
Ivaylo763 <iedzhurelov24a@gmail.com>
2026-01-22 09:47:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,16 @@ if age >= 18:
   st.write("Ти си пълнолетен")
 else:
   st.write("Ти си непълнолетен")
+
+  if grade < 3:
+    st.write("Слаб(2)")
+  if grade >= 3 && <3.5:
+    st.write("Среден(3)")
+  if grade >= 3.5 && grade < 4.5:
+    st.write("Добър(4)")
+  if grade >= 4.5 && grade < 5.5:
+    st.write("Много добър(5)")
+  if grade >= 5.5:
+    st.write("Отличен(6)")
+
+  
