32557c39e1ab31abcdc9d282ddbcdd2df16087ff
Ivaylo763 <iedzhurelov24a@gmail.com>
2026-01-22 09:39:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ name = st.text_input("Въведи име")
 age = st.text_input("Въведи възраст")
 grade = st.text_input("Въведи оценка")
 
-if age > 18:
+if age >= 18:
   st.write("Ти си пълнолетен")
 else:
   st.write("Ти си непълнолетен")
